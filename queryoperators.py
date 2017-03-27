9067d6c9097a06ab6bc33c7149d17556237eb43d
Ikechi <consumerike@gmail.com>
2017-03-27 11:54:23 -0500

    logical disjunction

--- queryoperators.py
+++ queryoperators.py
@@ -13,8 +13,15 @@ cursor = db.restaurants.find({"grades.score": {"$gt": 30}})
 #less than query:
 cursor = db.restaurants.find({"grades.score": {"$lt": 10}})
 
-logical and is executed with a comma separating a list of query conditions:
+# logical and is executed with a comma separating a list of query conditions:
 cursor = db.restaurants.find({"cuisine": "Italian", "address.zipcode": "10075"})
+
+# logical or is executed with $or:
+cursor = db.restaurants.find({"$or": [{"cuisine": "Italian"}, {"address.zipcode": "10075"}]})
+
+
+
+
 
 
 
