bbf5bde5ac7c9ea681c9380acea576822247bbab
Ikechi <consumerike@gmail.com>
2017-03-27 11:50:34 -0500

    added greater than and less than examples

--- queryoperators.py
+++ queryoperators.py
@@ -6,6 +6,13 @@ db = client.test
 #cursor for querying with operators:
 # General pattern:
 { <field1>: { <operator1>: <value1> } }
+
+#greater than query
 cursor = db.restaurants.find({"grades.score": {"$gt": 30}})
+
+#less than query:
+cursor = db.restaurants.find({"grades.score": {"$lt": 10}})
+
+
 
 
